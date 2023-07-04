289c01a2c4d375938afcb0e4c5c248cf815e0f07
deser56 <86250972+deser56@users.noreply.github.com>
2023-07-04 20:12:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ordrs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-5qcadn)
---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ObjectId } = require('mongodb');
+
+const connectionString = 'mongodb+srv://your-connection-string';
+const dbName = 'your-database-name';
+const collectionName = 'orders';
+
+let db;
+
+async function connectToDatabase() {
+  try {
+    const client = new MongoClient(connectionString);
+    await client.connect();
+    db = client.db(dbName);
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+}
+
+app.use(express.json());
+
+app.get('/orders', async (req, res) => {
+  try {
+    const orders = await db.collection(collectionName).find().toArray();
+    res.json(orders);
+  } catch (error) {
+    console.error('Error retrieving orders:', error);
+    res.sendStatus(500);
+  }
+});
+
+app.get('/orders/:id', async (req, res) => {
+  const orderId = req.params.id;
+  try {
+    const order = await db.collection(collectionName).findOne({
+      _id: ObjectId(orderId),
+    });
+    if (order) {
+      res.json(order);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    console.error('Error retrieving order:', error);
+    res.sendStatus(500);
+  }
+});
+
+app.post('/orders', async (req, res) => {
+  const { customerId, products } = req.body;
+  try {
+    const result = await db.collection(collectionName).insertOne({
+      customerId,
+      products,
+      status: 'Pending',
+    });
+    res.status(201).json({ _id: result.insertedId });
+  } catch (error) {
+    console.error('Error creating order:', error);
+    res.sendStatus(500);
+  }
+});
+
+app.put('/orders/:id', async (req, res) => {
+  const orderId = req.params.id;
+  const { status } = req.body;
+  try {
+    const result = await db.collection(collectionName).updateOne(
+      { _id: ObjectId(orderId) },
+      { $set: { status } }
+    );
+    if (result.modifiedCount > 0) {
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    console.error('Error updating order:', error);
+    res.sendStatus(500);
+  }
+});
+
+connectToDatabase().then(() => {
+  app.listen(3000, () => {
+    console.log('Order management service is running on port 3000');
+  });
+});
